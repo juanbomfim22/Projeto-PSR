78a1dbf9452f4b5f66075312d2b05f347d28b45a
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 19:05:31 -0300

    lol

---
+++ foo/oi.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function oi() {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
