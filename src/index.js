f5b4ba00f0ed7803196432b21e682c50662e7de9
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 21:28:23 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -1,38 +1,24 @@
-// const db = require('./config/db')
+const db = require('./config/db')
 
-// const express = require('express')
-// const app = express()
-
-// const path = require('path');
-// const consign = require('consign')
-
-// consign({ cwd: path.join(__dirname)})
-//     .include('./config/passport.js')
-//     .then('./config/middlewares.js')
-//     .then('./api')
-//     .then('./config/routes.js')
-//     .into(app)
-
-// app.db = db
-// app.use(express.static('public'))
-
-// const PORT = process.env.PORT || 3000
-
-// app.listen(PORT, () => {
-//     console.log(`Backend executando... na porta ${PORT}`)
-// })
-
-const express = require("express")
+const express = require('express')
 const app = express()
 
-// use the express-static middleware
-app.use(express.static("public"))
+const path = require('path');
+const consign = require('consign')
 
-// define the first route
-app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+consign({ cwd: path.join(__dirname)})
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
+app.db = db
+app.use(express.static('public'))
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Backend executando... na porta ${PORT}`)
 })
 
-// start the server listening for requests
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
