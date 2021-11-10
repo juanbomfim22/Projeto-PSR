ff6f70478e910317fcd940f608e1e99190eb933b
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 22:02:06 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -34,7 +34,7 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>Hello World!" + process.cwd() + "</h1>")
 })
 
 // start the server listening for requests
