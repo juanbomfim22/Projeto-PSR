e1cdc85e3834d9fb4c96880cc6862746a9f7b4bd
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 21:06:19 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -6,14 +6,14 @@ const app = express()
 const path = require('path');
 const consign = require('consign')
 
-// consign({ cwd: path.join(__dirname)})
-//     .include('./config/passport.js')
-//     .then('./config/middlewares.js')
-//     .then('./api')
-//     .then('./config/routes.js')
-//     .into(app)
+consign({ cwd: path.join(__dirname)})
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
 
-// app.db = db
+app.db = db
 app.use(express.static('public'))
 
 const PORT = process.env.PORT || 3000
