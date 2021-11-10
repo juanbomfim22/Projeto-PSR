8a6602971adba382bcc9818676e245cc2271eee5
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 21:54:13 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -11,17 +11,15 @@ const app = express()
 // No heroku
 const CWD = process.cwd() + '/app'
 
-consign({ cwd: CWD })
-    .include('./config/passport.js')
-    .then('./config/middlewares.js')
-    .then('./api')
-    .then('./config/routes.js')
-    .into(app)
+// consign({ cwd: CWD })
+//     .include('./config/passport.js')
+//     .then('./config/middlewares.js')
+//     .then('./api')
+//     .then('./config/routes.js')
+//     .into(app)
 
-
-// use the express-static middleware
 // app.use(express.static("public"))
 app.get('/', (req, res) => res.send(`<h1>${CWD}</h1>`))
-// start the server listening for requests
+
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
