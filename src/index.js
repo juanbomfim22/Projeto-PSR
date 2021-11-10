d100101f31e569d95daba3cd891a287623f12d2e
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 21:51:34 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -1,42 +1,26 @@
-// const db = require('./config/db')
-
-// const express = require('express')
-// const app = express()
-
-// const consign = require('consign')
-
-// // Na app local
-// // const path = require('path')
-// // const CWD = path.join(__dirname)
-
-// // No heroku
-// const CWD = process.cwd() + '/app'
-
-// // consign({ cwd: CWD })
-// //     .include('./config/passport.js')
-// //     .then('./config/middlewares.js')
-// //     .then('./api')
-// //     .then('./config/routes.js')
-// //     .into(app)
-
-// app.use(express.static('public'))
-
-// const PORT = process.env.PORT || 3000
-
-// app.listen(PORT, () => {
-//     console.log(`Backend executando... na porta ${PORT}`)
-// })
-
+const db = require('./config/db')
+const consign = require('consign')
 const express = require("express")
+
 const app = express()
+
+// Na app local
+// const path = require('path')
+// const CWD = path.join(__dirname)
+
+// No heroku
+const CWD = process.cwd() + '/app/src'
+
+consign({ cwd: CWD })
+    .include('./config/passport.js')
+    .then('./config/middlewares.js')
+    .then('./api')
+    .then('./config/routes.js')
+    .into(app)
+
 
 // use the express-static middleware
 app.use(express.static("public"))
-
-// define the first route
-app.get("/users", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
-})
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
