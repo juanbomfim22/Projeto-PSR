13778ada2e820b0cefd42842eea22ad21ffd24bb
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 22:25:13 -0300

    Fix paths

--- src/index.js
+++ src/index.js
@@ -8,12 +8,12 @@ const app = express()
 // use the express-static middleware
 app.use(express.static("public"))
 
-// consign()
-//     .include('src/config/passport.js')
-//     .then('src/config/middlewares.js')
-//     .then('src/api')
-//     .then('src/config/routes.js')
-//     .into(app)
+consign()
+    .include('src/config/passport.js')
+    .then('src/config/middlewares.js')
+    .then('src/api')
+    .then('src/config/routes.js')
+    .into(app)
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
