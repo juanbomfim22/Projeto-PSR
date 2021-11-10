01559f5f7364fe181a0bea71fff7b5d3a52e3cda
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 22:01:31 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -1,27 +1,42 @@
-const db = require('./config/db')
-// const consign = require('consign')
-const express = require("express")
+// const db = require('./config/db')
+// // const consign = require('consign')
+// const express = require("express")
 
+// const app = express()
+
+// // Na app local
+// // const path = require('path')
+// // const CWD = path.join(__dirname)
+
+// // No heroku
+// // const CWD = process.cwd() + '/app'
+
+// // consign({ cwd: CWD })
+// //     .include('./config/passport.js')
+// //     .then('./config/middlewares.js')
+// //     .then('./api')
+// //     .then('./config/routes.js')
+// //     .into(app)
+
+// // app.use(express.static("public"))
+// app.get("/", function (req, res) {
+//     res.send("<h1>Hello World!</h1>")
+//   })
+
+// app.listen(process.env.PORT || 3000, 
+// 	() => console.log("Server is running..."));
+
+const express = require("express")
 const app = express()
 
-// Na app local
-// const path = require('path')
-// const CWD = path.join(__dirname)
+// use the express-static middleware
+app.use(express.static("public"))
 
-// No heroku
-// const CWD = process.cwd() + '/app'
-
-// consign({ cwd: CWD })
-//     .include('./config/passport.js')
-//     .then('./config/middlewares.js')
-//     .then('./api')
-//     .then('./config/routes.js')
-//     .into(app)
-
-// app.use(express.static("public"))
+// define the first route
 app.get("/", function (req, res) {
-    res.send("<h1>Hello World!</h1>")
-  })
-  
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
