87b6000aa30ba282b0b42437c4def400e39bc7a0
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 22:03:01 -0300

    Fix port

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const express = require("express")
 const app = express()
 
 // use the express-static middleware
-app.use(express.static("public"))
+// app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
