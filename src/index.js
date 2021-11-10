9f0b828eb4e900115f54ec00d6b6d07e75f867f8
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 22:26:31 -0300

    Fix paths

--- src/index.js
+++ src/index.js
@@ -9,13 +9,13 @@ const app = express()
 app.use(express.static("public"))
 
 consign()
-    .include('src/config/passport.js')
-    .then('src/config/middlewares.js')
-    .then('src/api')
-    .then('src/config/routes.js')
+    .include('./src/config/passport.js')
+    .then('./src/config/middlewares.js')
+    .then('./src/api')
+    .then('./src/config/routes.js')
     .into(app)
 
-    
+
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
