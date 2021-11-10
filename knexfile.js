3a9826da4f738b627162968f6047fef94571d0ec
juanbomfim22 <juanbomfim22@gmail.com>
2021-11-09 23:35:26 -0300

    Fix paths

--- knexfile.js
+++ knexfile.js
@@ -3,14 +3,15 @@ module.exports = {
   client: 'postgresql',
   connection: {
     host: "ec2-34-194-100-156.compute-1.amazonaws.com",
-    database: 'd2gb9kahmibeco',
+    database: 'd2gb9kahmibeco?ssl=true',
     user: 'uxtysgehfovjzp',
     password: 'a4b68b2e58a73302f32fd091ff85dd5d7b25d7ea68e1e577af7415fa35230afe',
   },
   pool: { 
     min: 2,
     max: 10
-  }
+  },
+  ssl
 }; 
  
 // LOCAL
